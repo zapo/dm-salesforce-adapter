63bbc98cbc6577dbbe8f01d51e58f5658f80fa0c
Antoine Niek <antoine.niek@adgear.com>
2013-07-27 11:15:47 +0200

    updates to savonrb 2 syntax

--- lib/dm-salesforce-adapter/connection.rb
+++ lib/dm-salesforce-adapter/connection.rb
@@ -1,5 +1,5 @@
 class SalesforceAdapter
-  
+
   class Connection
     include Errors
 
@@ -7,23 +7,27 @@ class SalesforceAdapter
 
     def initialize(username, password, wsdl_path, organization_id = nil)
       @username, @password, @organization_id = URI.unescape(username), password, organization_id
-      @driver = Savon::Client.new(wsdl_path)
+
+      @driver = Savon.client do
+        wsdl wsdl_path
+      end
+
       @descriptions = {}
       login
     end
-    
+
     def description model
       storage_name = storage_name(model)
       @descriptions[storage_name.to_sym] ||= describe(storage_name)
     end
-    
+
     def storage_name model
       model = case
       when model.kind_of?(String)                            then return model
       when model.kind_of?(::DataMapper::Resource)            then model.class
       when model.is_a?(Class)                                then model
       end
-      
+
       model.storage_name(model.repository.name)
     end
 
@@ -32,7 +36,7 @@ class SalesforceAdapter
       id_prop = resource.class.properties.find {|p| p.name == :Id}
 
       return id_prop.get(resource) if id_prop
-      
+
       query_string = "SELECT Id FROM #{storage_name(resource.class)}"
       query_string << " WHERE #{resource.class.key.map {|k| "#{k.field} = #{k.get(resource)}"}.join(') AND (')} LIMIT 1"
 
@@ -46,29 +50,24 @@ class SalesforceAdapter
       field = field_for(model, column)
       field[:name] if field
     end
-    
-    
+
+
     def field_for(model, column)
       fields = [column, Inflector.camelize(column.to_s), "#{Inflector.camelize(column.to_s)}__c", "#{column}__c".downcase]
       options = /^(#{fields.join("|")})$/i
-      
+
       field = description(model).find {|col| col[:name].to_s.match(options) }
-      
+
       return field
-      
-      raise FieldNotFound,
-        "You specified #{column} as a field, but none of the expected field names exist: #{fields.join(", ")}. " \
-        "Either manually specify the field name with :field, or check to make sure you have " \
-        "provided a correct field name."
     end
-    
-    
+
+
     def session_headers ns = 'wsdl'
       {"#{ns}:SessionHeader" => {"#{ns}:sessionId" => @session_id}}
     end
-    
+
     def login_scope_headers ns = 'wsdl'
-      {"#{ns}:LoginScopeHeader" => {"#{ns}:organizationId" => @organization_id}} 
+      {"#{ns}:LoginScopeHeader" => {"#{ns}:organizationId" => @organization_id}}
     end
 
     def query(string)
@@ -76,25 +75,25 @@ class SalesforceAdapter
         soap.header = session_headers
         soap.body = {:queryString => string}
       end
-      
+
       result.to_hash[:query_response][:result]
     end
-    
+
     def query_more(locator)
       result = driver.request :query_more do
         soap.header = session_headers
         soap.body = {:queryLocator => locator}
       end
-      
+
       result.to_hash[:query_more_response][:result]
     end
-    
+
     def describe(klass_name)
       field_map = {
         :id                                    => ::DataMapper::Property::Serial,
-        :string                                => ::DataMapper::Property::String, 
-        :reference                             => ::DataMapper::Property::String, 
-        :phone                                 => ::DataMapper::Property::String, 
+        :string                                => ::DataMapper::Property::String,
+        :reference                             => ::DataMapper::Property::String,
+        :phone                                 => ::DataMapper::Property::String,
         :url                                   => ::DataMapper::Property::String,
         :textarea                              => ::DataMapper::Property::Text,
         :boolean                               => ::DataMapper::Property::Boolean,
@@ -109,16 +108,16 @@ class SalesforceAdapter
       end
 
       fields = result.to_hash[:describe_s_object_response][:result][:fields]
-      
+
       fields.each do |f|
         type = f[:type].to_s.to_sym
         type = (field_map[type] if field_map.has_key?(type)) || ::DataMapper::Property::String
         f[:dmtype] = type
       end
-      
+
       fields
     end
-    
+
     def prepare_resources resources
 
       resources.map(&:class).uniq.compact.each {|klass| description(klass)}
@@ -127,7 +126,7 @@ class SalesforceAdapter
           description(r.parent_model.storage_name)
       end
     end
-    
+
     def with_sf_limits_for resources, &block
       result = []
       resources.each_slice(200).to_a.each do |res|
@@ -135,37 +134,37 @@ class SalesforceAdapter
       end
       result.flatten
     end
-    
+
     def updatables? resources
       resources.each do |resource|
         raise FieldNotFound.new('Id', resource) unless resource.respond_to? :Id
         resource[:Id] = sf_id_for(resource)
       end
       true
-    rescue FieldNotFound => e
+    rescue FieldNotFound
       false
     end
-    
+
     def upsertables? resources
       resources.all? {|r| description(r.class).any? {|col| col[:external_id] == true }}
     rescue
       false
     end
-    
+
     def builder_for resources, method
       builder = if upsertables?(resources)
         UpsertResourcesBuilder
       else
         method == :update ? UpdateResourcesBuilder : CreateResourcesBuilder
       end
-      
+
       raise 'No builder found' unless builder
       builder
     end
 
     def create(resources)
       prepare_resources(resources)
-      
+
       builder = builder_for resources, :create
       method  = builder == UpsertResourcesBuilder ? :upsert : :create
 
@@ -181,19 +180,19 @@ class SalesforceAdapter
 
     def update(attributes, resources)
       prepare_resources(resources)
-      
+
       builder = builder_for resources, :update
       method  = builder == UpsertResourcesBuilder ? :upsert : :update
-      
+
       with_sf_limits_for resources do |res|
         res.each do |resource|
           resource[:Id] = sf_id_for(resource) if method == :update
         end
-        
+
         call_api(method, &builder.new({:resources => res, :attributes => attributes}, self))
       end
     end
-    
+
     def delete(collection)
       with_sf_limits_for collection do |resources|
         keys  = resources.map {|r| sf_id_for r }
@@ -207,7 +206,7 @@ class SalesforceAdapter
     def login
       username, password = @username, @password
 
-      result = driver.request :login do
+      result = driver.call :login do
         soap.body = {:username => username, :password => password}
       end
 
@@ -215,7 +214,7 @@ class SalesforceAdapter
       response.each do |k, v|
         instance_variable_set("@#{k}", v)
       end
-      
+
       driver.wsdl.endpoint = @server_url
       driver
     end
@@ -223,29 +222,29 @@ class SalesforceAdapter
     def call_api(method, args = nil, &block)
 
       body = args || block
-      
+
       if body.kind_of? Proc
         body = body.call(Builder::XmlMarkup.new).to_s
       end
-      
+
       result = driver.request method.to_sym do
         soap.header = session_headers
         soap.body = body
       end
-      
+
       hash_result = result.to_hash
       response = hash_result["#{method}_response".to_sym]
       response = [response[:result]].flatten if response && response[:result]
-      
+
       raise "Unhandled response: #{result.to_xml}, was expecting a '#{method}_response'" unless response
-      
+
       if response.all? {|r| r[:success]}
         response
       else
         response.reject {|r| r[:success]}.map {|r| r[:errors]}.flatten.each do |e|
-          
+
           status_code = e[:status_code].underscore.classify
-          
+
           if !Errors.const_defined?(status_code)
             Errors.const_set(status_code, Class.new(SOAPError))
           end
@@ -256,14 +255,14 @@ class SalesforceAdapter
     rescue Savon::SOAP::Fault => soap_fault
        message = soap_fault.message
        status_code = soap_fault.class.name.split(':').last.underscore.classify
-       
+
        if !Errors.const_defined?(status_code)
          Errors.const_set(status_code, Class.new(SOAPError))
        end
-        
+
        raise Errors.const_get(status_code).new(method, message)
     end
-    
+
     def with_reconnection(&block)
       yield
     end
