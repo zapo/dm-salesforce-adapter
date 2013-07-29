85c3853c40fe459e48f94caea7885a9777df206b
Antoine Niek <antoine.niek@adgear.com>
2013-07-29 17:22:03 +0200

    dont enforce serial sf length

--- lib/dm-salesforce-adapter/property/serial.rb
+++ lib/dm-salesforce-adapter/property/serial.rb
@@ -3,10 +3,8 @@ module SalesforceAdapter::Property
     accept_options :serial
     serial true
 
-    length 15
-
     def dump(value)
-      value[0..14] unless value.blank?
+      value unless value.blank?
     end
   end
 end
